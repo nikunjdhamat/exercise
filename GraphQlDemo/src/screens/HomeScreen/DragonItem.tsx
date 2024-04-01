8c74327d8a614dbea620e30b443e9378ba0e22e9
Nikunj Dhamat <nikunjbhai.dhamat@nagarro.com>
2024-04-01 12:57:38 +0530

    optional chaining added

--- GraphQlDemo/src/screens/HomeScreen/DragonItem.tsx
+++ GraphQlDemo/src/screens/HomeScreen/DragonItem.tsx
@@ -11,23 +11,23 @@ const DragonItem = ({item}: {item: Dragon}) => {
       <View style={[styles.itemRowContainer]}>
         <View style={styles.itemAlignStart}>
           <Text style={styles.titleTextStyle}>{t('dragon.name')}</Text>
-          <Text style={styles.valueTextStyle}>{item.name}</Text>
+          <Text style={styles.valueTextStyle}>{item?.name}</Text>
         </View>
         <View style={styles.itemAlignEnd}>
           <Text style={styles.titleTextStyle}>{t('dragon.diameter')}</Text>
-          <Text style={styles.valueTextStyle}>{item.diameter.feet}</Text>
+          <Text style={styles.valueTextStyle}>{item?.diameter?.feet}</Text>
         </View>
       </View>
 
       <View style={[styles.itemRowContainer, styles.topMargin]}>
         <View style={styles.itemAlignStart}>
           <Text style={styles.titleTextStyle}>{t('dragon.firstFlight')}</Text>
-          <Text style={styles.valueTextStyle}>{item.first_flight}</Text>
+          <Text style={styles.valueTextStyle}>{item?.first_flight}</Text>
         </View>
         <View style={styles.itemAlignEnd}>
           <Text style={styles.titleTextStyle}>{t('dragon.payloadMass')}</Text>
           <Text style={styles.valueTextStyle}>
-            {item.launch_payload_mass.lb}
+            {item?.launch_payload_mass?.lb}
           </Text>
         </View>
       </View>
