7df0493c03ce4c0979aa59e1b39f12df6d10ddc7
Nikunj Dhamat <nikunjbhai.dhamat@nagarro.com>
2024-04-02 11:44:05 +0530

    coverage report

--- MovieList/jest.config.js
+++ MovieList/jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: ['./setup-tests.js'],
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text'],
 };
